89efd8a4f8d5ec9503272158fff8658a17193ab8
angelaajiao <121437129+angelaajiao@users.noreply.github.com>
2026-01-23 20:26:01 +0100

    welcome screen complete

--- Sleep/src/navigation/RootNavigator.tsx
+++ Sleep/src/navigation/RootNavigator.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
+import WelcomeScreen from "../screens/WelcomeScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,18 +11,12 @@ export default function RootNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName ="Login"
-        screenOptions={{headerShown:false}}
-        
+        initialRouteName="Welcome"
+        screenOptions={{ headerShown: false }}
       >
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-        />
-        <Stack.Screen
-          name="Register"
-          component={RegisterScreen}
-        />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
